9a49c2d4b1bbb8f88fa56a6bce9781f3add2c2c1
Nguyễn Duy Chiến <100187821+Woofy2205@users.noreply.github.com>
2025-01-12 01:32:33 +0700

    Implement get_embeddings function in LocalEmbedding class and add test notebook for embedding functionality

--- embeddings/LocalEmbedding.py
+++ embeddings/LocalEmbedding.py
@@ -1,15 +1,40 @@
+import os
+import logging
+import sys
+import nest_asyncio
+import json
+import torch
+
 import numpy as np
 from typing import List, Any
 from llama_index.core.embeddings import BaseEmbedding
 
+from dotenv import load_dotenv
+from llama_index.llms.openai import OpenAI
+from llama_index.core import (
+    Settings,
+    Document, get_response_synthesizer,
+    KnowledgeGraphIndex, PropertyGraphIndex,
+    StorageContext
+)
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.graph_stores.neo4j import Neo4jGraphStore
+
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer("Alibaba-NLP/gte-large-en-v1.5", trust_remote_code=True, model_kwargs={"torch_dtype":"bfloat16"})
+
 def get_embeddings(text: str,
                    model: str,
-                   **kwargs) -> np.ndarray:
+                   **kwargs) -> List:
     """
     Get embeddings for a given text using a local pre-trained model.
     """
     # TODO: Implement this function
-    pass
+    _model = SentenceTransformer(model, trust_remote_code=True, model_kwargs={"torch_dtype":"bfloat16"})
+    embeddings = _model.encode(text, batch_size=32, show_progress_bar=True)
+    embeddings = embeddings.tolist()
+    return embeddings
 
 class LocalEmbedding(BaseEmbedding):
     """
